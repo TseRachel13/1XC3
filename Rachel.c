3ef2622d3a3966ca5e98c3f36bcd8e16657dd4ef
TseRachel <racheltse13@hotmail.com>
2023-06-28 12:52:40 -0400

    first commit

---
+++ Rachel.c
@@ -0,0 +1,6 @@
+# include <stdio.h>
+int main () {
+int x ;
+int y = x + 5; // Using uninitialized variable x
+printf ( "% d \n " , y ) ;
+}
---
+++ constantvariable.c
@@ -0,0 +1,5 @@
+# include <stdio.h>
+int main () {
+const int MAX_VALUE = 100;
+printf ( " Max value : % d\n " , MAX_VALUE ) ;;
+}
